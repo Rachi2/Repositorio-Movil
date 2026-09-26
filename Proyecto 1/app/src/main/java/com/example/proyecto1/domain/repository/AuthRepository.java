a31b60806692f5e79d0c706df1abadec272df859
Yuji Yamaki <YXYK0001@ce.pucmm.edu.do>
2026-09-26 05:04:12 -0400

    Creacion de la interfaz de AuthRepository

---
+++ Proyecto 1/app/src/main/java/com/example/proyecto1/domain/repository/AuthRepository.java
@@ -0,0 +1,13 @@
+package com.example.proyecto1.domain.repository;
+
+import com.example.proyecto1.domain.model.User;
+
+public interface AuthRepository {
+    void register(String email, String password, ChatRepository.RepositoryCallback<User> callback);
+
+    void login(String email, String password, ChatRepository.RepositoryCallback<User> callback);
+
+    void logout();
+
+    User getCurrentUser();
+}
